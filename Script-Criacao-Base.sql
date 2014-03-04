eaee6e9f7616a66efc1c2e0d015668c082272cfb
andrefarnezi <de_baterasp@hotmail.com>
2014-03-04 13:17:11 -0300

    Alteracao Base

--- Script-Criacao-Base.sql
+++ Script-Criacao-Base.sql
@@ -4,6 +4,6 @@ GO
 CREATE DATABASE [P3ImageTeste] ON  PRIMARY 
 ( 
     NAME = N'P3ImageTeste', 
-    FILENAME = N'D:\teste\P3Image-teste\P3ImageTeste.mdf' ,			--local data path
+    FILENAME = N'D:\P3Image-teste\P3ImageTeste.mdf' ,
     SIZE = 8000KB , MAXSIZE = UNLIMITED, FILEGROWTH = 1024KB 
 )
